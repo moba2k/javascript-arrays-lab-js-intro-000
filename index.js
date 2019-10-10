a9b049edc50af53f9c9c1846a4d2c8dd931ffc2a
Mobasher Sobhan <moba2k@yahoo.com>
2019-10-10 03:56:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -45,15 +45,8 @@ function appendKitten (name='Broom') {
 
 var kittens = ["Milo","Otis","Garfield"]; //define your array here
 
-<<<<<<< HEAD
 // Add your functions and code here - prepend return no change - spread
 
 function prependKitten (name) {
   return [name, ...kittens];
-=======
-// Add your functions and code here - prepend return no change
-
-function prependKitten () {
-  return = [name, ...kittens];
->>>>>>> 14b2c82c672123e495e9650657084fd742a280e7
 }
