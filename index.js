cbcfeafd2824add16b879334192f5d15e68bbccf
Mobasher Sobhan <moba2k@yahoo.com>
2019-10-10 04:09:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ var kittens = ["Milo","Otis","Garfield"]; //define your array here
 // Add your functions and code here - rem last kitten - return new, no change - slice
 
 function removeLastKitten () {
-  kittens.slice (0, kittens.length - 1);
+  return kittens.slice (0, kittens.length - 1);
 }
 
 
